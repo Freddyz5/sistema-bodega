1142668bda77c9f7081b0765258895e79426c3e8
MarceSeg <erika.segarra.87@est.ucacue.edu.ec>
2026-07-18 13:04:13 -0500

    Creacion de servicio para consumir datos de inventario desde la API.

--- src/Inventario/Inventario.tsx
+++ src/Inventario/Inventario.tsx
@@ -1,3 +1,4 @@
 //Inicializar pagina para inventario, añadir componentes base de barra de navegacion y barra lateral.
 //Anadir componente de tabla
 //Añadir columnas a la tabla: Id, Nombre, Descripcion, Cantidad, Precio, Fecha de ingreso, Fecha de vencimiento, Acciones (Editar y Eliminar).
+//Creacion de servicio para consumir datos de inventario desde la API.
