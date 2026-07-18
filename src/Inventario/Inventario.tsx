161b88d0f7d04e95b67bf3df0cbb41a2e3f3fd72
MarceSeg <erika.segarra.87@est.ucacue.edu.ec>
2026-07-18 13:11:49 -0500

    Se agrega funcionalidad para los filtros, permitiendo filtrar por cantidad, precio y fecha de ingreso.

--- src/Inventario/Inventario.tsx
+++ src/Inventario/Inventario.tsx
@@ -6,3 +6,4 @@
 //Correccion de error en la tabla al no mostrar los datos correctamente, se aumenta la columna de proveedores.
 //Servicio de inventario actualizado para incluir proveedores en la respuesta de la API.
 //Se agrega funcionalidad de busqueda en la tabla de inventario, permitiendo filtrar por nombre y descripcion.
+//Se agrega funcionalidad para los filtros, permitiendo filtrar por cantidad, precio y fecha de ingreso.
