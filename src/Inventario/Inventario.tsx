b2a0f576ad9c94d5ec51d0967f583b71375e29c9
MarceSeg <erika.segarra.87@est.ucacue.edu.ec>
2026-07-18 13:04:57 -0500

    Creacion de hook para consumir el servicio de inventario y manejar el estado de los datos.

--- src/Inventario/Inventario.tsx
+++ src/Inventario/Inventario.tsx
@@ -2,3 +2,4 @@
 //Anadir componente de tabla
 //Añadir columnas a la tabla: Id, Nombre, Descripcion, Cantidad, Precio, Fecha de ingreso, Fecha de vencimiento, Acciones (Editar y Eliminar).
 //Creacion de servicio para consumir datos de inventario desde la API.
+//Creacion de hook para consumir el servicio de inventario y manejar el estado de los datos.
