94175d4f980b279cd30f7c41c3c6b50970b51c4a
MarceSeg <erika.segarra.87@est.ucacue.edu.ec>
2026-07-18 13:02:18 -0500

    Añadir columnas a la tabla: Id, Nombre, Descripcion, Cantidad, Precio, Fecha de ingreso, Fecha de vencimiento, Acciones (Editar y Eliminar).

--- src/Inventario/Inventario.tsx
+++ src/Inventario/Inventario.tsx
@@ -1,2 +1,3 @@
 //Inicializar pagina para inventario, añadir componentes base de barra de navegacion y barra lateral.
 //Anadir componente de tabla
+//Añadir columnas a la tabla: Id, Nombre, Descripcion, Cantidad, Precio, Fecha de ingreso, Fecha de vencimiento, Acciones (Editar y Eliminar).
