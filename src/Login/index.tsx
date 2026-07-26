875b4f9bdb3bf858326acd357e5ddced4c5c99c8
MarceSeg <erika.segarra.87@est.ucacue.edu.ec>
2026-07-26 18:53:37 -0500

    feat: Implementar autenticación en dos factores (2FA)

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -3,3 +3,4 @@
 //Se mejora la apariencia del formulario con CSS para facilitar la interacción del usuario.
 //Se implementa la validación para evitar el envío del formulario con campos vacíos.
 //Se agregan reglas para exigir longitud mínima, mayúsculas, minúsculas, números y caracteres especiales.
+//Después de validar usuario y contraseña, el sistema solicita un código de verificación enviado al teléfono móvil.
