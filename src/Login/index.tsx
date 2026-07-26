9fb1c027c63eec5fe8022a651b8e374b9c4f3ed7
MarceSeg <erika.segarra.87@est.ucacue.edu.ec>
2026-07-26 18:51:57 -0500

    feat: Agregar validación de campos obligatorios

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,3 +1,4 @@
 //Creacion del proyecto
 //Se incorpora el formulario con campos Usuario y Contraseña y el botón Iniciar sesión.
 //Se mejora la apariencia del formulario con CSS para facilitar la interacción del usuario.
+//Se implementa la validación para evitar el envío del formulario con campos vacíos.
