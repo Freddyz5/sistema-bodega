9341e39ef142a2b2ca8fe53d49ddd0b9d6c272d9
MarceSeg <erika.segarra.87@est.ucacue.edu.ec>
2026-07-26 18:51:00 -0500

    style: Aplicar estilos al formulario de autenticación

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,2 +1,3 @@
 //Creacion del proyecto
 //Se incorpora el formulario con campos Usuario y Contraseña y el botón Iniciar sesión.
+//Se mejora la apariencia del formulario con CSS para facilitar la interacción del usuario.
